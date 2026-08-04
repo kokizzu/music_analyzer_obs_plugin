14c087c3fbdd1d549397c1b221296c5b75a14d91
Kiswono Prayogo <kiswono@gmail.com>
2026-08-04 15:48:37 +0700

    Add OBS Unicode title overlay interface

---
+++ src/audacious_overlay.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cstdint>
+
+struct obs_source;
+typedef struct obs_source obs_source_t;
+
+namespace mao {
+
+struct AnalysisSnapshot;
+struct VisualizerRenderer;
+struct AudaciousUnicodeOverlay;
+
+void render_visualizer_with_audacious(VisualizerRenderer *visualizer, const AnalysisSnapshot &snapshot,
+				      float snapshot_age);
+
+AudaciousUnicodeOverlay *create_audacious_unicode_overlay(obs_source_t *parent);
+void destroy_audacious_unicode_overlay(AudaciousUnicodeOverlay *overlay);
+void tick_audacious_unicode_overlay(AudaciousUnicodeOverlay *overlay);
+void render_audacious_unicode_overlay(AudaciousUnicodeOverlay *overlay, uint32_t width, uint32_t height);
+
+} // namespace mao
