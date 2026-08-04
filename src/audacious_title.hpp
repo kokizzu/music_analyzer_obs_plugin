55ef69bfdb0a9efb398442700f46eab3a764c75a
Kiswono Prayogo <kiswono@gmail.com>
2026-08-04 14:38:35 +0700

    Fix Audacious window title parsing

--- src/audacious_title.hpp
+++ src/audacious_title.hpp
@@ -11,6 +11,7 @@ struct AudaciousNowPlaying {
 	std::string song_title;
 };
 
+std::string find_audacious_window_title(const std::string &wmctrl_output);
 std::string extract_audacious_song_title(const std::string &titlebar);
 std::string make_scrolling_title(const std::string &title, std::size_t width, std::size_t offset);
 AudaciousNowPlaying read_audacious_now_playing();
