dae8d20e40cd1e1c9611a79975845c2293d18916
Kiswono Prayogo <kiswono@gmail.com>
2026-08-04 15:53:00 +0700

    Expose Audacious draw wrapper

--- src/audacious_overlay.hpp
+++ src/audacious_overlay.hpp
@@ -4,6 +4,8 @@
 
 struct obs_source;
 typedef struct obs_source obs_source_t;
+struct gs_texture;
+typedef struct gs_texture gs_texture_t;
 
 namespace mao {
 
@@ -18,5 +20,8 @@ AudaciousUnicodeOverlay *create_audacious_unicode_overlay(obs_source_t *parent);
 void destroy_audacious_unicode_overlay(AudaciousUnicodeOverlay *overlay);
 void tick_audacious_unicode_overlay(AudaciousUnicodeOverlay *overlay);
 void render_audacious_unicode_overlay(AudaciousUnicodeOverlay *overlay, uint32_t width, uint32_t height);
+
+void audacious_obs_source_draw(gs_texture_t *texture, uint32_t x, uint32_t y, uint32_t width, uint32_t height,
+			      bool flip);
 
 } // namespace mao
