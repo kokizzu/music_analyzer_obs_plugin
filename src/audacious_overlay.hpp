e9cb881af8cfd43ec4b9736cfefebd90cb05515e
Kiswono Prayogo <kiswono@gmail.com>
2026-08-04 16:00:18 +0700

    Align Audacious draw wrapper declaration

--- src/audacious_overlay.hpp
+++ src/audacious_overlay.hpp
@@ -21,6 +21,7 @@ void destroy_audacious_unicode_overlay(AudaciousUnicodeOverlay *overlay);
 void tick_audacious_unicode_overlay(AudaciousUnicodeOverlay *overlay);
 void render_audacious_unicode_overlay(AudaciousUnicodeOverlay *overlay, uint32_t width, uint32_t height);
 
-void audacious_obs_source_draw(gs_texture_t *texture, int x, int y, uint32_t width, uint32_t height, bool flip);
+void audacious_obs_source_draw(gs_texture_t *texture, uint32_t x, uint32_t y, uint32_t width, uint32_t height,
+			      bool flip);
 
 } // namespace mao
