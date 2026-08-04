c95e2a9b5a493b920c5a88c520fc06196055b98c
Kiswono Prayogo <kiswono@gmail.com>
2026-08-04 14:35:03 +0700

    Add Audacious title helpers

---
+++ src/audacious_title.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstddef>
+#include <string>
+
+namespace mao {
+
+struct AudaciousNowPlaying {
+	bool running = false;
+	std::string window_title;
+	std::string song_title;
+};
+
+std::string extract_audacious_song_title(const std::string &titlebar);
+std::string make_scrolling_title(const std::string &title, std::size_t width, std::size_t offset);
+AudaciousNowPlaying read_audacious_now_playing();
+
+} // namespace mao
